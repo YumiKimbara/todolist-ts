49ce9587b64257723764e7ce82c736977d340332
yumi0127 <a13158y@gmail.com>
2021-10-10 16:59:15 -0700

    create a handler

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
+import NewTodo from "./components/NewTodo";
 import Todos from "./components/Todos";
 import Todo from "./models/todo";
 
 function App() {
   const todos = [new Todo("learn react"), new Todo("learn typescript")];
+
+  const addTodoHandler = (todoText: string) => {};
   return (
     <div>
+      <NewTodo onAddTodo={addTodoHandler} />
       <Todos items={todos} />
     </div>
   );
--- src/components/NewTodo.tsx
+++ src/components/NewTodo.tsx
@@ -1,11 +1,24 @@
 import { useRef } from "react";
 
-const NewTodo = () => {
+//onAddTodo: (text: string) => voidはonAddTodoのtypeを指定している。
+//(text: string)=>voidは関数ではなく、onAddTodoが関数のtypeで何もreturnしない(voidのため)ということを表していてcallback関数ではない。
+const NewTodo: React.FC<{ onAddTodo: (text: string) => void }> = (props) => {
   //refがHTMLInputElementとconnectしていることを知らせる。
-  const todoTextInputRef = useRef<HTMLInputElement>();
+  const todoTextInputRef = useRef<HTMLInputElement>(null);
   //eventもTSはどんなタイプかわからないから、React.FormEventと指定してあげる。
   const submitHandler = (event: React.FormEvent) => {
     event.preventDefault();
+
+    //current.valueが存在するかtsはわからないため、?が自動的につき、もし成功すればvalueが、失敗すればnullが格納される。
+    //もしdeveloper自身が必ずvalueが格納されると確信しているなら?の代わりに!をつける。
+    // const enteredText = todoTextInputRef.current?.value;
+    const enteredText = todoTextInputRef.current!.value;
+
+    if (enteredText.trim().length === 0) {
+      return;
+    }
+
+    props.onAddTodo(enteredText);
   };
 
   return (
